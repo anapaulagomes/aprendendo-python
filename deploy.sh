c5dbae92dab8898b635628100869675df6124d79
Ana Paula Gomes <apgomes88@gmail.com>
2019-12-08 10:14:21 -0300

    Deploy em So 8. Dez 10:14:21 -03 2019

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,14 @@ python from_notebook_to_post.py
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
-# Add changes to git.
 git add .
 
-# Commit changes.
-msg="rebuilding site $(date)"
+msg="Deploy em $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
 git push --force-with-lease origin master
